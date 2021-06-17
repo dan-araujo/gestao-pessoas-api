88ad6a2f01915d7f10e14336138db290573ec3e5
Daniel Araújo <daniel.ti.info@gmail.com>
2021-06-17 15:29:42 -0300

    Classes utilitárias para os testes unitários

--- src/test/java/git/com/dan/araujo/gestao/pessoas/api/utils/PessoaUtils.java
+++ src/test/java/git/com/dan/araujo/gestao/pessoas/api/utils/PessoaUtils.java
@@ -1,2 +1,38 @@
-package git.com.dan.araujo.gestao.pessoas.api.utils;public class PessoaUtils {
+package git.com.dan.araujo.gestao.pessoas.api.utils;
+
+import git.com.dan.araujo.gestao.pessoas.api.dto.request.DTOPessoa;
+import git.com.dan.araujo.gestao.pessoas.api.entities.Pessoa;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PessoaUtils {
+
+    private static final String PRIMEIRO_NOME = "Danilo";
+    private static final String SOBRENOME = "Araújo";
+    private static final String NUMERO_CPF = "643.404.570-00";
+    private static final long ID_PESSOA = 1L;
+    public static final LocalDate DATA_NASCIMENTO = LocalDate.of(1997, 11, 1);
+
+    public static DTOPessoa criarDTOFake() {
+        return DTOPessoa.builder()
+                .primeiroNome(PRIMEIRO_NOME)
+                .sobrenome(SOBRENOME)
+                .cpf(NUMERO_CPF)
+                .dataNascimento("04-04-1997")
+                .telefones(Collections.singletonList(TelefoneUtils.criarDTOFake()))
+                .build();
+    }
+
+    public static Pessoa criarEntidadeFake() {
+        return Pessoa.builder()
+                .id(ID_PESSOA)
+                .primeiroNome(PRIMEIRO_NOME)
+                .sobrenome(SOBRENOME)
+                .cpf(NUMERO_CPF)
+                .dataNascimento(DATA_NASCIMENTO)
+                .telefones(Collections.singletonList(TelefoneUtils.criarEntidadeFake()))
+                .build();
+    }
+
 }
--- src/test/java/git/com/dan/araujo/gestao/pessoas/api/utils/TelefoneUtils.java
+++ src/test/java/git/com/dan/araujo/gestao/pessoas/api/utils/TelefoneUtils.java
@@ -1,2 +1,27 @@
-package git.com.dan.araujo.gestao.pessoas.api.utils;public class TelefoneUtils {
+package git.com.dan.araujo.gestao.pessoas.api.utils;
+
+import git.com.dan.araujo.gestao.pessoas.api.dto.request.DTOTelefone;
+import git.com.dan.araujo.gestao.pessoas.api.entities.Telefone;
+import git.com.dan.araujo.gestao.pessoas.api.enums.TipoDeTelefone;
+
+public class TelefoneUtils {
+
+    private static final String NUMERO_TELEFONE = "1199999-9999";
+    private static final TipoDeTelefone TIPO_DE_TELEFONE = TipoDeTelefone.CELULAR;
+    private static final long ID_TELEFONE = 1L;
+
+    public static DTOTelefone criarDTOFake() {
+        return DTOTelefone.builder()
+                .numero(NUMERO_TELEFONE)
+                .tipo(TIPO_DE_TELEFONE)
+                .build();
+    }
+
+    public static Telefone criarEntidadeFake() {
+        return Telefone.builder()
+                .id(ID_TELEFONE)
+                .numero(NUMERO_TELEFONE)
+                .tipo(TIPO_DE_TELEFONE)
+                .build();
+    }
 }
