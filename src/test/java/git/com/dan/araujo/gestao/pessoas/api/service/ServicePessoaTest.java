226bbecf6c70b1b00baea47778111a6e7553b186
Daniel Araújo <daniel.ti.info@gmail.com>
2021-06-17 15:27:23 -0300

    Service para pessoa

--- src/test/java/git/com/dan/araujo/gestao/pessoas/api/service/ServicePessoaTest.java
+++ src/test/java/git/com/dan/araujo/gestao/pessoas/api/service/ServicePessoaTest.java
@@ -1,6 +1,8 @@
 package git.com.dan.araujo.gestao.pessoas.api.service;
 
 import git.com.dan.araujo.gestao.pessoas.api.dto.request.DTOPessoa;
+import git.com.dan.araujo.gestao.pessoas.api.dto.response.DTORespostaDaMensagem;
+import git.com.dan.araujo.gestao.pessoas.api.entities.Pessoa;
 import git.com.dan.araujo.gestao.pessoas.api.repository.RepositorioPessoa;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,8 +10,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static git.com.dan.araujo.gestao.pessoas.api.utils.PessoaUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
-public class PersonServiceTest {
+public class ServicePessoaTest {
 
     @Mock
     private RepositorioPessoa repositorioPessoa;
@@ -18,8 +24,23 @@ public class PersonServiceTest {
     private ServicePessoa servicePessoa;
 
     @Test
-    void testeDTOParaRetornarMensagemSalvaParaDeterminadaPessoa() {
+    void testeDTOQueRetornaMensagemSalvaParaDeterminadaPessoa() {
         DTOPessoa dtoPessoa = criarDTOFake();
+        Pessoa pessoaEsperadaSalva = criarEntidadeFake();
+
+        when(repositorioPessoa.save(any(Pessoa.class))).thenReturn(pessoaEsperadaSalva);
+
+        DTORespostaDaMensagem mensagemDeSucessoEsperada = criarRespostaEsperadaDaMensagem(pessoaEsperadaSalva.getId());
+        DTORespostaDaMensagem mensagemDeSucesso = servicePessoa.criarPessoa(dtoPessoa);
+
+        assertEquals(mensagemDeSucessoEsperada, mensagemDeSucesso);
+    }
+
+    private DTORespostaDaMensagem criarRespostaEsperadaDaMensagem(Long id) {
+        return DTORespostaDaMensagem
+                .builder()
+                .mensagem("Pessoa criada com o ID " + id)
+                .build();
     }
 
 }
